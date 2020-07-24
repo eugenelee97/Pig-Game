0b7b306c9ec66f3a25baf7028c8d4c19b5867b76
eugenelee97 <pinkfjgh@gmail.com>
2020-07-24 22:24:21 +0900

    deleted unused codes

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-var scores, roundScore, activePlayer, gamePlaying, lastDice1, lastDice2, winningScore;
+var scores, roundScore, activePlayer, gamePlaying, winningScore;
 
 init();
 
@@ -19,34 +9,23 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
     if (gamePlaying && winningScore) {
 
-        console.log(winningScore);
         // 1. Random Number
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
 
         //2. Display the result
-        var diceDOM1 = document.querySelector('.dice1');
-        diceDOM1.style.display = 'block';
-        diceDOM1.src = 'img/dice-' + dice1 + '.png';
-
-        var diceDOM2 = document.querySelector('.dice2');
-        diceDOM2.style.display = 'block';
-        diceDOM2.src = 'img/dice-' + dice2 + '.png';
+        document.querySelector('.dice1').style.display = 'block';
+        document.querySelector('.dice2').style.display = 'block';
+        document.querySelector('.dice1').src = 'img/dice-' + dice1 + '.png';
+        document.querySelector('.dice2').src = 'img/dice-' + dice2 + '.png';
 
         //3, Update the round score IF the rolled numer was NOT a 1
         if ((dice1 !== 1) && (dice2 !== 1)) {
             roundScore += (dice1 + dice2);
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        }
-        else if ((dice1 == lastDice1 == 6) && (dice2 == lastDice2 == 6)) {
+        } else {
             nextPlayer();
         }
-        else {
-            nextPlayer();
-        }
-        lastDice1 = dice1;
-        lastDice2 = dice2;
-
     }
 
 });
@@ -114,5 +93,3 @@ function init() {
 
 
 }
-//document.querySelector('#current' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
